6abd606f5446f39e9a3be1b491073da444271c8e
Tuan Anh <tuan73176@gmail.com>
2026-04-18 04:44:57 +0000

    G8441-user 9 47.2.A.11.228 3311891731 release-keys
    Change-Id: Ia20379b2ba3d95a83fe9d98b485429e0a033178a

---
+++ lilac-vendor.mk
@@ -0,0 +1,104 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/sony/lilac
+
+PRODUCT_COPY_FILES += \
+    vendor/sony/lilac/proprietary/recovery/root/vendor/firmware/touch_module_id_0xf0.img:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/touch_module_id_0xf0.img \
+    vendor/sony/lilac/proprietary/recovery/root/vendor/firmware/touch_module_id_0xf1.img:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/touch_module_id_0xf1.img \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/color_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/color_ctrl.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/dataflow.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/dataflow.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/exposure_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/exposure_ctrl.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/exposure_ctrl_diagram.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/exposure_ctrl_diagram.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/exposure_ctrl_gamma.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/exposure_ctrl_gamma.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/face_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/face_detector.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/flicker_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/flicker_detector.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/focus_stats_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/focus_stats_conv.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/food_comp_yummy.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/food_comp_yummy.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/food_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/food_detector.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/hal_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/hal_ctrl.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/hal_ctrl_platform_abf.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/hal_ctrl_platform_abf.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/hal_ctrl_platform_asf_lin.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/hal_ctrl_platform_asf_lin.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/hal_ctrl_platform_asf_stp.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/hal_ctrl_platform_asf_stp.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/hal_ctrl_platform_eccc.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/hal_ctrl_platform_eccc.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/hal_ctrl_platform_gic.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/hal_ctrl_platform_gic.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/hal_ctrl_platform_ltm.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/hal_ctrl_platform_ltm.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/hal_ctrl_platform_nree.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/hal_ctrl_platform_nree.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/hdr_comp.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/hdr_comp.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/idt_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/idt_ctrl.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/image_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/image_conv.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/image_conv_canele.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/image_conv_canele.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/image_conv_cheesescone.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/image_conv_cheesescone.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/image_conv_dars.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/image_conv_dars.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/image_conv_marble.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/image_conv_marble.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/image_conv_raisin.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/image_conv_raisin.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/image_conv_spica.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/image_conv_spica.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/lens_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/lens_ctrl.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/motion_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/motion_detector.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/motion_detector_sumomo.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/motion_detector_sumomo.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/motion_estimation.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/motion_estimation.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/multiframe_nr.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/multiframe_nr.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/multiframe_nr_melonpan.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/multiframe_nr_melonpan.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/multiframe_nr_mugichoco.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/multiframe_nr_mugichoco.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/multiframe_nr_toppo.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/multiframe_nr_toppo.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/object_tracker.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/object_tracker.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/prc_image_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/prc_image_conv.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/CHI08BS0/scene_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/CHI08BS0/scene_detector.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/color_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/color_ctrl.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/dataflow.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/dataflow.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/exposure_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/exposure_ctrl.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/exposure_ctrl_diagram.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/exposure_ctrl_diagram.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/exposure_ctrl_gamma.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/exposure_ctrl_gamma.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/face_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/face_detector.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/flicker_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/flicker_detector.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/focus_stats_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/focus_stats_conv.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/food_comp_yummy.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/food_comp_yummy.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/food_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/food_detector.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/hal_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/hal_ctrl.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/hal_ctrl_platform_abf.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/hal_ctrl_platform_abf.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/hal_ctrl_platform_asf_lin.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/hal_ctrl_platform_asf_lin.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/hal_ctrl_platform_asf_stp.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/hal_ctrl_platform_asf_stp.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/hal_ctrl_platform_eccc.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/hal_ctrl_platform_eccc.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/hal_ctrl_platform_gic.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/hal_ctrl_platform_gic.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/hal_ctrl_platform_ltm.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/hal_ctrl_platform_ltm.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/hal_ctrl_platform_nree.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/hal_ctrl_platform_nree.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/hdr_comp.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/hdr_comp.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/idt_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/idt_ctrl.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/image_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/image_conv.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/image_conv_canele.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/image_conv_canele.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/image_conv_cheesescone.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/image_conv_cheesescone.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/image_conv_dars.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/image_conv_dars.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/image_conv_marble.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/image_conv_marble.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/image_conv_raisin.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/image_conv_raisin.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/image_conv_spica.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/image_conv_spica.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/lens_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/lens_ctrl.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/motion_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/motion_detector.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/motion_detector_sumomo.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/motion_detector_sumomo.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/motion_estimation.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/motion_estimation.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/multiframe_nr.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/multiframe_nr.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/multiframe_nr_melonpan.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/multiframe_nr_melonpan.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/multiframe_nr_mugichoco.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/multiframe_nr_mugichoco.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/multiframe_nr_toppo.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/multiframe_nr_toppo.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/object_tracker.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/object_tracker.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/prc_image_conv.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/prc_image_conv.dat \
+    vendor/sony/lilac/proprietary/vendor/camera/LGI08BS0/scene_detector.dat:$(TARGET_COPY_OUT_VENDOR)/camera/LGI08BS0/scene_detector.dat \
+    vendor/sony/lilac/proprietary/vendor/etc/ad_calib.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/ad_calib.cfg \
+    vendor/sony/lilac/proprietary/vendor/etc/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
+    vendor/sony/lilac/proprietary/vendor/etc/qdcm_calib_data_5.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_5.xml \
+    vendor/sony/lilac/proprietary/vendor/etc/qdcm_calib_data_8.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_8.xml \
+    vendor/sony/lilac/proprietary/vendor/etc/sensors/sensor_def_somc_product.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_somc_product.conf \
+    vendor/sony/lilac/proprietary/vendor/etc/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    vendor/sony/lilac/proprietary/vendor/etc/touch/images/big_touch.png:$(TARGET_COPY_OUT_VENDOR)/etc/touch/images/big_touch.png \
+    vendor/sony/lilac/proprietary/vendor/etc/touch/images/splash.png:$(TARGET_COPY_OUT_VENDOR)/etc/touch/images/splash.png \
+    vendor/sony/lilac/proprietary/vendor/etc/wifi/bdwlan.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bdwlan.bin \
+    vendor/sony/lilac/proprietary/vendor/firmware/touch_module_id_0xf0.img:$(TARGET_COPY_OUT_VENDOR)/firmware/touch_module_id_0xf0.img \
+    vendor/sony/lilac/proprietary/vendor/firmware/touch_module_id_0xf1.img:$(TARGET_COPY_OUT_VENDOR)/firmware/touch_module_id_0xf1.img \
+    vendor/sony/lilac/proprietary/vendor/firmware/tzfingerprint.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/tzfingerprint.b01 \
+    vendor/sony/lilac/proprietary/vendor/firmware/tzfingerprint.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/tzfingerprint.mdt \
+    vendor/sony/lilac/proprietary/vendor/usr/idc/clearpad.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/clearpad.idc
+
+PRODUCT_PACKAGES += \
+    lib_fpc_tac_shared \
+    fpc_fingerprint@2.1_HIDL-service
